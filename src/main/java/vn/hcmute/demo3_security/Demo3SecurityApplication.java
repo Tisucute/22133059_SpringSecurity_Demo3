3c370c74376d7a3a02567d7cbfabcf4e422f6339
Tisucute <ngocanhthugialai@gmail.com>
2024-11-20 10:48:12 +0700

    demo3

---
+++ src/main/java/vn/hcmute/demo3_security/Demo3SecurityApplication.java
@@ -0,0 +1,12 @@
+package vn.hcmute.demo3_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo3SecurityApplication.class, args);
+	}
+}
